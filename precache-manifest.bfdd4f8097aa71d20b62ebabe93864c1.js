6123b8e7a38bcaaa5d37be77c45e04db0a63fe8d
Af009 <afwebd@yahoo.com>
2018-10-25 17:29:15 +0300

    Updates

---
+++ precache-manifest.bfdd4f8097aa71d20b62ebabe93864c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fffea918c9fe3298796",
+    "url": "/box-shadows/static/js/runtime~main.5fffea91.js"
+  },
+  {
+    "revision": "4b5283e221fea6774e34",
+    "url": "/box-shadows/static/js/main.4b5283e2.chunk.js"
+  },
+  {
+    "revision": "a234e70b512e8a596b71",
+    "url": "/box-shadows/static/js/1.a234e70b.chunk.js"
+  },
+  {
+    "revision": "4b5283e221fea6774e34",
+    "url": "/box-shadows/static/css/main.3740799e.chunk.css"
+  },
+  {
+    "revision": "a234e70b512e8a596b71",
+    "url": "/box-shadows/static/css/1.226734af.chunk.css"
+  },
+  {
+    "revision": "e84cfbaec90ad1e95aca9c7f5b0dc2e4",
+    "url": "/box-shadows/index.html"
+  }
+];
